855f98545189a0579fa6cf7e6d1497660a15e593
nawfal-code <nawfal.mohammed006@gmail.com>
2025-06-27 12:08:22 +0530

    Create Product.java

---
+++ Solution1.2/Product.java
@@ -0,0 +1,13 @@
+package com.guvi.task2;
+//create class with parameterized constructor
+public class Product {
+    String pid;
+    double price;
+    double quantity;
+    public Product(String pid,double price,double quantity){
+        this.pid=pid;
+        this.price=price;
+        this.quantity=quantity;
+    }
+
+}
